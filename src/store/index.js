2891745ed0c6a6172d6f1c1567173036e0c59d1f
ckliao <ckliao@g.nccu.edu.tw>
2021-10-17 17:15:29 +0800

    Add multiple state islogin, username, game_id, theme and corresponding mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,28 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		islogin: false,
+		username: "",
+		game_id: "",
+		theme: "light",
+	},
+	mutations: {
+		login_signout (state, in_out){
+			state.islogin = in_out;
+		},
+		changeName (state, name){
+			state.username = name;
+		},
+		changeTheme (state, theme){
+			state.theme = theme;
+		},
+		changeGameId (state, game_id){
+			state.game_id = game_id;
+		}
+	},
+	actions: {
+	},
+	modules: {
+	}
 })

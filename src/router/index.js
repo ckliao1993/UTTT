804f74c6a8cbce6bf1af4a2f0eaa21e0968addad
ckliao <ckliao@g.nccu.edu.tw>
2021-10-18 21:03:39 +0800

    Router setting + 404 - about

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/games',
@@ -25,10 +16,14 @@ const routes = [
     name: 'Play',
     component: () => import('../views/Play.vue')
   },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('../views/404.vue')
+  },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ src/views/404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main d-flex flex-column align-items-center justify-content-center">
+    <h1>404 Not Found.</h1>
+  </div>
+</template>

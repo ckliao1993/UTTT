e2a8f0680412c7c9ef997090bed46a118f76b34c
ckliao <ckliao@g.nccu.edu.tw>
2021-10-19 00:14:54 +0800

    back 2 hash mode...

--- src/router/index.js
+++ src/router/index.js
@@ -6,24 +6,28 @@ const routes = [
     redirect: {name: "Home"}
   },
   {
-    path: '/ooxx/',
+    path: '/',
     name: 'Home',
     component: () => import('../views/Home.vue')
   },
   {
-    path: '/ooxx/games',
+    path: '/games',
     name: 'Games',
     component: () => import('../views/404.vue')
   },
   {
-    path: '/ooxx/play/:game_id',
+    path: '/play/:game_id',
     name: 'Play',
     component: () => import('../views/Play.vue')
-  }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('../views/404.vue')
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
